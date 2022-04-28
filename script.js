ef1355daf1dfab0e6a22a64f651ab94f0a071532
Santiago <sanramu93@gmail.com>
2022-04-28 14:40:48 -0500

    Add image carousel

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: false,
+const swiperWrapper = document.querySelector(".swiper-wrapper");
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+const getImageUrl = async () => {
+  try {
+    const res = await fetch(`https://picsum.photos/1080/566`);
+    return res.url;
+  } catch {
+    console.error("Not image found");
+  }
+};
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+const getImages = async (amount) => {
+  const images = [];
+  for (let i = 0; i < amount; i++) {
+    const image = await getImageUrl();
+    images.push(image);
+  }
+  return images;
+};
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+const renderImages = async (amount = 4) => {
+  const images = await getImages(amount);
+  images.forEach((imgUrl) => {
+    const newSlide = document.createElement("div");
+    const newImage = document.createElement("img");
+    newSlide.classList.add("swiper-slide");
+    newImage.src = imgUrl;
+    newImage.alt = "Image";
+    newSlide.appendChild(newImage);
+    swiperWrapper.appendChild(newSlide);
+  });
+
+  // SWIPER
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+  return swiper;
+};
+
+renderImages(4);
